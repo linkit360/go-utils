d9ac5e3cda865d4d13e8869c89ef605c4728e2b8
Helena Nevskaya <helena.nevskaya@ya.ru>
2017-02-01 11:19:28 +0300

    xmp_subscriptions partitioning

--- db/sql/partitioning/triggers.sql
+++ db/sql/partitioning/triggers.sql
@@ -43,3 +43,17 @@ drop trigger xmp_pixel_transactions_insert_trigger on xmp_pixel_transactions ;
 drop trigger xmp_operator_transaction_log_insert_trigger on xmp_operator_transaction_log ;
 drop trigger xmp_campaigns_access_insert_trigger on xmp_campaigns_access ;
 drop trigger xmp_transaction_insert_trigger on xmp_transactions ;
+
+
+
+SELECT
+  nmsp_parent.nspname AS parent_schema,
+  parent.relname      AS parent,
+  nmsp_child.nspname  AS child_schema,
+  child.relname       AS child
+FROM pg_inherits
+  JOIN pg_class parent            ON pg_inherits.inhparent = parent.oid
+  JOIN pg_class child             ON pg_inherits.inhrelid   = child.oid
+  JOIN pg_namespace nmsp_parent   ON nmsp_parent.oid  = parent.relnamespace
+  JOIN pg_namespace nmsp_child    ON nmsp_child.oid   = child.relnamespace
+WHERE parent.relname='xmp_subscriptions';
