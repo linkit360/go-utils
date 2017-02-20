deb9cfd686999d99f296b39149c7ffccd36da61d
Helena Nevskaya <helena.nevskaya@ya.ru>
2017-02-20 11:36:42 +0300

    v1.7.4

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -5,7 +5,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var VersionName = "1.7.3"
+var VersionName = "1.7.4"
 
 func AddHandler(r *gin.Engine) {
 	_ = r.Group("/metrics").GET("", Version, gin.WrapH(prometheus.Handler()))
